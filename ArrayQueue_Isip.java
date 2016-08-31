bfc18b79779acdb8917470fc98187161624abc9a
Michael John Isip <mj.isip123@gmail.com>
2016-08-31 18:17:10 +0800

    Add files via upload

---
+++ ArrayQueue_Isip.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MJ
+ */
+public class ArrayQueue_Isip {
+
+    private String[] arrayQueue;
+    private int arraySize, head=0, tail=0;
+
+    public ArrayQueue_Isip(int size) {
+        arraySize = size;
+        arrayQueue = new String[arraySize];
+    }
+
+    public static void main(String[] args){
+      ArrayQueue_Isip cons = new ArrayQueue_Isip(10);
+      System.out.println("Queue Size: " + cons.arraySize);
+
+      //show on empty queue
+      cons.Show();
+      
+      cons.Enqueue("one");
+      cons.Enqueue("two");
+      cons.Enqueue("three");
+      cons.Enqueue("four");
+      cons.Enqueue("five");
+      cons.Enqueue("six");
+      cons.Enqueue("seven");
+      cons.Enqueue("eight");
+      cons.Enqueue("nine");
+      cons.Enqueue("ten");
+      
+      cons.Show();
+      
+      //enqueue on full queue
+      cons.Enqueue("eleven");
+      
+      //dequeue last element
+      cons.Dequeue();
+      
+      //peel last element
+      cons.Peek();
+      
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+      cons.Dequeue();
+
+      cons.Show();
+      
+      //Dequeue on empty Queue
+      cons.Dequeue();
+}    
+
+    public void Show(){
+        System.out.println();        
+        for(int i=0; i<arraySize; i++){
+            System.out.println("Queue [" + i + "] = " + arrayQueue[i]);
+        }
+        System.out.println();
+    }
+    
+    public boolean IsEmpty(){
+        if((tail == -1) && (head == -1)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean IsFull(){
+        if(arraySize-1 == tail){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void Enqueue(String input){
+        if(IsFull()){
+            System.out.println("Queue is FULL. Failed enqueue..");
+        } else if (IsEmpty()){
+            head =0;
+            tail=0;
+            arrayQueue[tail] = input;
+        } else {
+            tail++;
+            arrayQueue[tail] = input;
+        }
+    }
+    
+    public void Dequeue(){
+        if(IsEmpty()){
+            System.out.println("Queue is EMPTY. Failed dequeue..");
+        } else if(head == tail){
+            arrayQueue[head] = null;
+            head= -1;
+            tail = -1;
+            System.out.println();
+            System.out.println("Dequeue successful..");
+        } else {
+            arrayQueue[head] = null;
+            head ++;
+            System.out.println();
+            System.out.println("Dequeue successful..");
+        }
+    }
+    
+    public void Peek(){
+        System.out.println();
+        System.out.println("Queue [" + tail + "] = " + arrayQueue[tail]);
+    }
+    
+}
---
+++ ArrayStack_Isip.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MJ
+ */
+public class ArrayStack_Isip {
+    
+  private String[] arrayStack;
+  private int arraySize;
+  private int top=0;
+    
+    
+  public ArrayStack_Isip(int size) {
+      arraySize = size;
+      arrayStack = new String[arraySize];
+  }
+  public static void main(String[] args) {
+      ArrayStack_Isip cons = new ArrayStack_Isip(10);
+      
+      System.out.println("STACK SIZE = " + cons.arraySize);
+      System.out.println();
+
+      //Show while stack is Empty
+      cons.Show();
+      
+      //Push an element
+      cons.Push("mj");
+      
+      //Peek top element
+      cons.Peek();
+      
+      //Pop an element
+      cons.Pop();
+      
+      cons.Show();
+      
+      //Peek on Empty stack
+      cons.Peek();
+      
+      //Pop on Empty stack
+      cons.Pop();
+      
+      cons.Push("one");
+      cons.Push("two");
+      cons.Push("three");
+      cons.Push("four");
+      cons.Push("five");
+      cons.Push("six");
+      cons.Push("seven");
+      cons.Push("eight");
+      cons.Push("nine");
+      cons.Push("ten");
+      
+      //Push on Full stack
+      cons.Push("eleven");
+  }
+  
+  public void Show(){
+      for(int i=arraySize-1; i>=0; i--){
+          System.out.println("Stack [" + i + "] = " + arrayStack[i]);
+      }
+      System.out.println();      
+  }
+  
+  public void Peek(){
+      if(IsEmpty()){
+          System.out.println("Stack is EMPTY!");
+          System.out.println();
+      } else {
+          System.out.println("TOP: Stack [" + (top-1) + "] = " + arrayStack[top-1]);          
+          System.out.println();
+      }
+  }
+  
+  public void Push(String input){
+      if (IsFull()){
+          System.out.println("Cannot PUSH anymore. Stack is FULL!");
+          System.out.println();
+      } else {
+          arrayStack[top] = input;
+          top++;
+          System.out.println("Stack [" + (top-1) + "] = " + arrayStack[top-1]);
+          System.out.println("--- Successful PUSH.");          
+          System.out.println();
+      }
+  }
+  
+  public void Pop(){
+      if(IsEmpty()){
+          System.out.println("Cannot POP an element. Stack is EMPTY!");
+          System.out.println();
+      } else {
+          System.out.println("Stack [" + (top-1) + "] = " + arrayStack[top-1]);
+          arrayStack[top-1] = null;
+          top--;
+          System.out.println("--- Successful POP.");
+          System.out.println();
+      }
+  }
+  
+  public boolean IsEmpty(){
+      if (top == 0){
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  public boolean IsFull(){
+      if(top == arraySize){
+          return true;
+      } else {
+          return false;
+      }
+  }
+}
