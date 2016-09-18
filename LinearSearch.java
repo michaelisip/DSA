6dfaa6ac7d87001c6fc9f689eab03a796d9eb6e7
Michael John Isip <mj.isip123@gmail.com>
2016-09-18 19:09:17 +0900

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinearSearch;
+
+/**
+ *
+ * @author MJ
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class LinearSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    int array[];
+    
+    public void LinearSearch(int size){
+        array = new int[size];
+    }
+    
+    public void Random(int size){
+        Random randNum = new Random();
+        for(int i=0; i<array.length; i++){
+            array[i] = randNum.nextInt(500);
+        }
+    }
+    
+    public void Show(){
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i] + "\t");
+        }
+    }
+    
+    public void Search(int search, int size){
+        for(int i=0; i<array.length; i++){
+            if(array[i] == search){
+                System.out.println("Element found on index " + i);
+                break;
+            }
+        }
+        System.out.println("Element not found.");
+    }
+    
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        LinearSearch obj = new LinearSearch();
+        
+        System.out.println("Enter array size: ");
+        int size = input.nextInt();
+        
+        System.out.println("Element to be searched: ");
+        int search = input.nextInt();
+        
+        obj.Show();
+        obj.Search(search, size);
+    }
+
+}
